375746c777b01f92cb1b0a7859b3ee0a18b546b4
chiri03 <lena.kiriac.97@gmail.com>
2023-12-04 22:31:19 +0200

    update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const button = document.querySelector(".button");
-const jokeText = document.querySelector(".dadJoke");
+let jokeSetup = document.querySelector("#jokeSetup");
+let jokeContent = document.querySelector("#jokeContent");
 
 document.addEventListener("DOMContentLoaded", getJoke);
 button.addEventListener("click", getJoke);
 
 async function getJoke() {
-  const jokeData = await fetch("https://icanhazdadjoke.com/", {
+  const jokeData = await fetch("https://v2.jokeapi.dev/joke/Christmas", {
     headers: {
       Accept: "application/json",
     },
   });
   const jokeObj = await jokeData.json();
-  jokeText.innerHTML = jokeObj.joke;
+  jokeSetup.textContent = jokeObj.setup;
+  jokeContent.textContent = jokeObj.delivery + " 😂";
 }
